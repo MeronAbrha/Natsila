65ed9c7d6d446cb100c19956c240256ffa72c006
DolleyNolawi <mer.world@gmail.com>
2019-01-21 13:44:27 -0600

    work with fixing scss files and routing

--- natsila-tigrigna/src/app/app-routing.module.ts
+++ natsila-tigrigna/src/app/app-routing.module.ts
@@ -1,10 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HelloComponent } from './hello/hello.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: '/hello', pathMatch: 'full' },
+  { path: 'hello', component: HelloComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
+
