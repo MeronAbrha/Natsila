9db0f2b6738cfa295c54406f48bb666e0de15d1e
DolleyNolawi <mer.world@gmail.com>
2019-02-09 01:01:16 -0600

    modify interface and make youtube api call test

--- natsila-tigrigna/src/app/hello/hello.component.ts
+++ natsila-tigrigna/src/app/hello/hello.component.ts
@@ -8,16 +8,26 @@ import {DataService } from './../services/data.service';
 })
 export class HelloComponent implements OnInit {
    
+  items: any;
+  vidId = [];
+  vidTitle = [];
+  outputs = [];
   constructor(
     private _dataservice : DataService
   ){}
 
   ngOnInit() {
 
-    this._dataservice.getplayListDetails();
+    this._dataservice.getplayListDetails().then(res => {
+      //console.log("test rest -->"+ res);
+      this.items = res["items"];
+      for(let i in this.items){
+        
+          this.vidTitle.push( this.items[i].snippet.title);
+          this.vidId.push(this.items[i].snippet.resourceId.videoId);
+          this.outputs.push( '<li><iframe src=\"//www.youtube.com/embed/' + this.items[i].snippet.resourceId.videoId + '\></frame></li>');
+      }
+    });
   }
-
-  //call for content details 
-
 
 }
--- natsila-tigrigna/src/app/services/data.service.ts
+++ natsila-tigrigna/src/app/services/data.service.ts
@@ -1,79 +1,4 @@
-import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-//import { Router } from '@angular/router';
-
-
-@Injectable({
-  providedIn: 'root'
-})
-//@Injectable()
-export class DataService {
-
-  constructor(  private http: HttpClient) {}
-
-  getplayListDetails(){
-    return this.http
-    .get('https://www.googleapis.com/youtube/v3/channels?part=contentDetails&id=UCJT3Lo_NZri-bHJXVuYv3ew&key=AIzaSyAWuk33_ilAg98GlctU-CjXLZdI53RydOs')
-    .toPromise()
-    .then((res: Response) => {
-      console.log(res);
-      for( let i in res.items){
-          var pid = res.items[i].contentDetails.relatedPlaylists.uploads;
-          this.getVids(pid);
-      }
-      debugger;
-      //return res;
-    });
-  }
-
-  getVids(pid) {
-     return this.http
-     .get('https://www.googleapis.com/youtube/v3/playlistItems?part=snippet&maxResults=25&playlistId=' + pid + '&key=AIzaSyAWuk33_ilAg98GlctU-CjXLZdI53RydOs')
-     .toPromise()
-     .then((res: Response) => {
-       console.log(res);
-       debugger;
-  //     for( let i in res.items){
-  //         var pid = res.items[i].contentDetails.relatedPlaylists.uploads;
-  //         getVids(pid);
-       });
-  }
-  
-  getPlayList(): Promise<any> {
-    //getPlayList(requestParams: any, forUserName:any , key: any): Promise<any> {
-    
-
-    /* youtube api call sample:
-    GET {base_URL}/playlists?part=snippet
-                        &channelId=UC_x5XG1OV2P6uZZ5FSM9Ttw
-                        &key={YOUR_API_KEY}
-    */
-
-    //https://www.googleapis.com/youtube/v3/channels?part=contentDetails&forUsername=UCJT3Lo_NZri-bHJXVuYv3ew&key=AIzaSyAWuk33_ilAg98GlctU-CjXLZdI53RydOs
-
-    let reqParam = {
-      part: 'contentDetails',
-      forUserName: 'UCJT3Lo_NZri-bHJXVuYv3ew',
-      key: 'AIzaSyAWuk33_ilAg98GlctU-CjXLZdI53RydOs'
-    }
-
-    
-    return this.http.get('https://www.googleapis.com/youtube/v3/channels')
-      .toPromise()
-      .then((res: Response) => {
-        return res;
-      })
-      .catch((error: any) => {
-        return Promise.reject();
-      });
-   
-  }
-
-}
-
 /*
-  READ ME HOW TO MAKE A CALL TO YOUTUBE API 
-
   First get the id of the uploaded play list
   GET https://www.googleapis.com/youtube/v3/channels?part=contentDetails&id=CHANNEL_ID&key=API_KEY
 
@@ -83,7 +8,58 @@ export class DataService {
 */ 
 
 
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+//@Injectable()
+export class DataService {
+
+  constructor(  private http: HttpClient) {}
+
+  URL ='https://www.googleapis.com/youtube/v3/';
+
+  getplayListDetails(){
+
+    return this.http
+      .get( this.URL + 'channels?part=contentDetails&id=UCJT3Lo_NZri-bHJXVuYv3ew&key=AIzaSyAWuk33_ilAg98GlctU-CjXLZdI53RydOs')
+      .toPromise()
+      .then((res: Response) => {
+        let playListUpload = res["items"];
+        console.log(playListUpload);
+        for( let i in playListUpload){
+            var upload_id = playListUpload[i].contentDetails.relatedPlaylists.uploads;
+            return this.getVids(upload_id);
+        }
+    });
+   }
+
+  getVids(uploads) {
+     return this.http
+     .get( this.URL + 'playlistItems?part=snippet&maxResults=25&playlistId=' + uploads + '&key=AIzaSyAWuk33_ilAg98GlctU-CjXLZdI53RydOs')
+     .toPromise()
+     .then((res: Response) => {
+       console.log(res);
+       return res;
+  //     debugger;
+  //     for( let i in res.items){
+  //         var pid = res.items[i].contentDetails.relatedPlaylists.uploads;
+  //         getVids(pid);
+       });
+  }
+}
 
 /* ref site 
   https://stackoverflow.com/questions/31140083/youtube-api-returning-wrong-result-counts
+
+  MIX-CLOUND: 
+  https://www.mixcloud.com/discover/amharic/
+
+
+  Developers:
+  https://www.mixcloud.com/developers/
+   
 */
+
